dfccb546355db3fac320e2cdcdd7d49c886877b8
Mel <melissa.sargent@hotmail.co.uk>
2022-01-11 21:15:51 +0000

    working

--- WordGuess/Program.cs
+++ WordGuess/Program.cs
@@ -6,7 +6,34 @@ namespace WordGuess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string secretword = "blue";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretword && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter colour: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You lose!");
+            }
+            else
+            {
+                Console.Write("You Win!");
+            }
+
         }
     }
 }
